d4d02f693bd8581292ef972149eea72c19bdd2b5
Kirill Maksimov <kirill@orbs.com>
2019-02-06 13:32:37 +0200

    Fix ethereum and node count

--- resources/terraform/aws/ethereum-ebs-import.tf
+++ resources/terraform/aws/ethereum-ebs-import.tf
@@ -1,9 +1,11 @@
 resource "aws_ebs_volume" "ethereum" {
+  count             = "${var.ethereum_count}"
   availability_zone = "${var.aws_ether_az}"
   size              = 100
 }
 
 resource "aws_volume_attachment" "ethereum_storage_attachment" {
+  count       = "${var.ethereum_count}"
   device_name = "/dev/sdh"
   volume_id   = "${aws_ebs_volume.ethereum.id}"
   instance_id = "${aws_instance.ethereum.id}"
